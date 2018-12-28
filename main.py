f82ffd32c0f4459d3f180c67d45fe91da83ac336
takahiro-itazuri <takahiro.lab.1226@gmail.com>
2018-12-29 00:15:06 +0900

    refactoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
 import sys
-
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -10,245 +11,260 @@ from torch.utils.data import TensorDataset, DataLoader
 
 from tensorboardX import SummaryWriter
 
-# === hyperparameter === #
-nz = 16
-nh = 8
-no = 2
-nf = 8
 
-gamma = 1.0
-nj = 10
+class JusticeGAN():
+	def __init__(self, opt):
+		self.opt = opt
+		
+		# === model === #
+		# Criminal (犯罪者)
+		self.C = nn.Sequential(
+			nn.Linear(opt.nz, opt.ncf),
+			nn.BatchNorm1d(opt.ncf),
+			nn.ReLU(True),
+			nn.Linear(opt.ncf, opt.ncf),
+			nn.BatchNorm1d(opt.ncf),
+			nn.ReLU(True),
+			nn.Linear(opt.ncf, opt.ncf),
+			nn.BatchNorm1d(opt.ncf),
+			nn.ReLU(True),
+			nn.Linear(opt.ncf, opt.ncf),
+			nn.BatchNorm1d(opt.ncf),
+			nn.ReLU(True),
+			nn.Linear(opt.ncf, 2)
+		).to(opt.device)
 
-use_gpu = True
-device = torch.device("cuda:0" if use_gpu else "cpu")
-num_epochs = 100
-num_test_samples = 80000
-lr = 0.1
-batch_size = 8000
+		# Offer (状況証拠)
+		self.O = nn.Sequential(
+			nn.Linear(2, opt.njf),
+			nn.BatchNorm1d(opt.njf),
+			nn.ReLU(True),
+			nn.Linear(opt.njf, opt.njf),
+			nn.BatchNorm1d(opt.njf),
+			nn.ReLU(True),
+		).to(opt.device)
 
-writer = SummaryWriter('runs/lr{:.3f}_epoch{:d}'.format(lr, num_epochs))
-output_filename = 'result_lr{:.3f}_epoch{:d}.png'.format(lr, num_epochs)
+		# Lawyer (弁護士)
+		self.L = nn.Sequential(
+			nn.Linear(opt.njf, opt.njf),
+			nn.BatchNorm1d(opt.njf),
+			nn.ReLU(True),
+			nn.Linear(opt.njf, opt.njf)
+		).to(opt.device)
 
-# === dataset === #
-radius = 1.0
-npoints = 8
-means = []
-for p in range(npoints):
-	theta = p * 2.0 * np.pi / npoints
-	means.append(torch.tensor([radius * float(np.cos(theta)), radius * float(np.sin(theta))]))
-std = 0.1
-nsamples = 10000
-data = []
-for i in range(npoints):
-	data.append(means[i] + std * torch.randn(nsamples, 2))
-tensor_data = torch.cat(data)
-np_data = tensor_data.numpy()
-dataset = TensorDataset(tensor_data)
-loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+		# Prosecutor (検察官)
+		self.P = nn.Sequential(
+			nn.Linear(opt.njf, opt.njf),
+			nn.BatchNorm1d(opt.njf),
+			nn.ReLU(True),
+			nn.Linear(opt.njf, opt.njf)
+		).to(opt.device)
 
-# === model === #
-# Criminal (犯罪者)
-C = nn.Sequential(
-  nn.Linear(nz, nh),
-  nn.BatchNorm1d(nh),
-  nn.ReLU(True),
-  nn.Linear(nh, nh),
-  nn.BatchNorm1d(nh),
-  nn.ReLU(True),
-  nn.Linear(nh, nh),
-  nn.BatchNorm1d(nh),
-  nn.ReLU(True),
-  nn.Linear(nh, nh),
-  nn.BatchNorm1d(nh),
-  nn.ReLU(True),
-  nn.Linear(nh, nh),
-  nn.BatchNorm1d(nh),
-  nn.ReLU(True),
-  nn.Linear(nh, nh),
-  nn.BatchNorm1d(nh),
-  nn.ReLU(True),
-  nn.Linear(nh, no),
-  nn.Tanh()
-).to(device)
+		# Judge (裁判官)
+		self.J = nn.Sequential(
+			nn.BatchNorm1d(2 * opt.njf),
+			nn.ReLU(True),
+			nn.Linear(2 * opt.njf, 1),
+			nn.Sigmoid()
+		).to(opt.device)
 
-# Offer (状況証拠)
-O = nn.Sequential(
-  nn.Linear(no, nf),
-  nn.BatchNorm1d(nf),
-  nn.ReLU(True),
-  nn.Linear(nf, nf),
-  nn.BatchNorm1d(nf),
-  nn.ReLU(True),
-  nn.Linear(nf, nf),
-  nn.BatchNorm1d(nf),
-  nn.ReLU(True),
-  nn.Linear(nf, nf),
-  nn.BatchNorm1d(nf),
-  nn.ReLU(True)
-).to(device)
+		# === criterion === #
+		self.criterion = nn.BCELoss()
 
-# Lawyer (弁護士)
-L = nn.Sequential(
-	nn.Linear(nf, nf),
-	nn.BatchNorm1d(nf),
-	nn.ReLU(True)
-).to(device)
+		# === optimizers === #
+		self.C_optimizer = optim.Adam(self.C.parameters(), lr=self.opt.lr, betas=(self.opt.beta1, self.opt.beta2))
+		self.O_optimizer = optim.Adam(self.O.parameters(), lr=self.opt.lr, betas=(self.opt.beta1, self.opt.beta2))
+		self.L_optimizer = optim.Adam(self.L.parameters(), lr=self.opt.lr, betas=(self.opt.beta1, self.opt.beta2))
+		self.P_optimizer = optim.Adam(self.P.parameters(), lr=self.opt.lr, betas=(self.opt.beta1, self.opt.beta2))
+		self.J_optimizer = optim.Adam(self.J.parameters(), lr=self.opt.lr, betas=(self.opt.beta1, self.opt.beta2))
 
-# Prosecutor (検察官)
-P = nn.Sequential(
-	nn.Linear(nf, nf),
-	nn.BatchNorm1d(nf),
-	nn.ReLU(True)
-).to(device)
+	def train(self):
+		self.C.train()
+		self.O.train()
+		self.L.train()
+		self.P.train()
+		self.J.train()
 
-# Judge (裁判官)
-J = nn.Sequential(
-	nn.Linear(2 * nf, 1),
-	nn.Sigmoid()
-).to(device)
+		C_running_loss = 0.0
+		O_running_loss = 0.0
+		L_running_loss = 0.0
+		P_running_loss = 0.0
+		J_running_loss = 0.0
 
-# === criterion === #
-criterion = nn.BCELoss()
+		nitrs = 0
 
-# === optimizers === #
-C_optimizer = optim.Adam(C.parameters(), lr=lr)
-O_optimizer = optim.Adam(O.parameters(), lr=lr)
-L_optimizer = optim.Adam(L.parameters(), lr=lr)
-P_optimizer = optim.Adam(P.parameters(), lr=lr)
-J_optimizer = optim.Adam(J.parameters(), lr=lr)
+		for itr, (x) in enumerate(self.opt.loader):
+			nitrs += 1
 
-real = 1
-fake = 0
+			# --- label --- #
+			real_label = torch.full((self.opt.batch_size, 1), 1, device=self.opt.device)
+			fake_label = torch.full((self.opt.batch_size, 1), 0, device=self.opt.device)
 
-# === training === #
-C.train()
-O.train()
-L.train()
-P.train()
-J.train()
+			# --- Update J network --- #
+			self.J_optimizer.zero_grad()
 
-for epoch in range(num_epochs):
-	C_running_loss = 0.0
-	O_running_loss = 0.0
-	L_running_loss = 0.0
-	P_running_loss = 0.0
-	J_running_loss = 0.0
+			x_real = x[0].to(self.opt.device)
+			v_real = self.O(x_real)
+			vp_real = self.P(v_real) + v_real
+			vl_real = self.L(v_real) + v_real
+			j_real = self.J(torch.cat([vp_real, vl_real], dim=1))
 
-	for itr, (x) in enumerate(loader):
-		# --- label --- #
-		real_label = torch.full((batch_size,1), real, device=device)
-		fake_label = torch.full((batch_size,1), fake, device=device)
+			z = torch.randn((self.opt.batch_size, self.opt.nz), device=self.opt.device)
+			x_fake = self.C(z)
+			v_fake = self.O(x_fake)
+			vp_fake = self.P(v_fake) + v_fake
+			vl_fake = self.L(v_fake) + v_fake
+			j_fake = self.J(torch.cat([vp_fake, vl_fake], dim=1))
 
-		# --- Update J network --- #
-		J_optimizer.zero_grad()
+			loss_real = self.criterion(j_real, real_label)
+			loss_fake = self.criterion(j_fake, fake_label)
+			J_loss = loss_real + loss_fake
+			J_loss.backward()
+			self.J_optimizer.step()
 
-		x_real = x[0].to(device)
-		v_real = O(x_real)
-		vp_real = P(v_real) + v_real
-		vl_real = L(v_real) + v_real
-		j_real = J(torch.cat([vp_real, vl_real], dim=1))
+			if (itr + 1) % self.opt.njitrs:
+				# --- Update P & L network --- #
+				self.P_optimizer.zero_grad()
+				self.L_optimizer.zero_grad()
 
-		z = torch.randn((batch_size, nz), device=device)
-		x_fake = C(z)
-		v_fake = O(x_fake)
-		vp_fake = P(v_fake) + v_fake
-		vl_fake = L(v_fake) + v_fake
-		j_fake = J(torch.cat([vp_fake, vl_fake], dim=1))
+				x_real = x[0].to(self.opt.device)
+				v_real = self.O(x_real)
+				vp_real = self.P(v_real) + v_real
+				vl_real = self.L(v_real) + v_real
+				j_real = self.J(torch.cat([vp_real, vl_real], dim=1))
 
-		loss_real = criterion(j_real, real_label)
-		loss_fake = criterion(j_fake, fake_label)
-		J_loss = loss_real + loss_fake
-		J_loss.backward()
-		J_optimizer.step()
+				z = torch.randn((self.opt.batch_size, self.opt.nz), device=self.opt.device)
+				x_fake = self.C(z)
+				v_fake = self.O(x_fake)
+				vp_fake = self.P(v_fake) + v_fake
+				vl_fake = self.L(v_fake) + v_fake
+				j_fake = self.J(torch.cat([vp_fake, vl_fake], dim=1))
 
-		# --- Update C network --- #
-		C_optimizer.zero_grad()
+				P_loss = torch.mean(j_real) + self.opt.gamma * torch.mean(vp_real**2) + self.opt.gamma * torch.mean(vp_fake**2)
+				# P_loss = j_real + j_fake
+				P_loss.backward(retain_graph=True)
+				self.P_optimizer.step()
 
-		z = torch.randn((batch_size, nz), device=device)
-		x_fake = C(z).detach()
-		v_fake = O(x_fake)
-		vp_fake = P(v_fake) + v_fake
-		vl_fake = L(v_fake) + v_fake
-		j_fake = J(torch.cat([vp_fake, vl_fake], dim=1))
+				L_loss = torch.mean(j_fake) + self.opt.gamma * torch.mean(vl_real**2) + self.opt.gamma * torch.mean(vl_fake**2)
+				# L_loss = j_real + j_fake
+				L_loss.backward(retain_graph=True)
+				self.L_optimizer.step()
 
-		loss_real = criterion(j_real, real_label)
-		loss_fake = criterion(j_fake, fake_label)
+				# --- Update O network --- #
+				self.O_optimizer.zero_grad()
 
-		C_loss = criterion(j_fake, real_label)
-		C_loss.backward()
-		C_optimizer.step()
+				x_real = x[0].to(self.opt.device)
+				v_real = self.O(x_real)
+				vp_real = self.P(v_real) + v_real
+				vl_real = self.L(v_real) + v_real
+				j_real = self.J(torch.cat([vp_real, vl_real], dim=1))
 
-		if itr % nj == 0:
-			# --- Update P & L network --- #
-			P_optimizer.zero_grad()
-			L_optimizer.zero_grad()
+				z = torch.randn((self.opt.batch_size, self.opt.nz), device=self.opt.device)
+				x_fake = self.C(z)
+				v_fake = self.O(x_fake)
+				vp_fake = self.P(v_fake) + v_fake
+				vl_fake = self.L(v_fake) + v_fake
+				j_fake = self.J(torch.cat([vp_fake, vl_fake], dim=1))
 
-			x_real = x[0].to(device)
-			v_real = O(x_real)
-			vp_real = P(v_real) + v_real
-			vl_real = L(v_real) + v_real
-			j_real = J(torch.cat([vp_real, vl_real], dim=1))
+				loss_real = self.criterion(j_real, real_label)
+				loss_fake = self.criterion(j_fake, fake_label)
 
-			z = torch.randn((batch_size, nz), device=device)
-			x_fake = C(z)
-			v_fake = O(x_fake)
-			vp_fake = P(v_fake) + v_fake
-			vl_fake = L(v_fake) + v_fake
-			j_fake = J(torch.cat([vp_fake, vl_fake], dim=1))
+				O_loss = loss_real + loss_fake
+				O_loss.backward()
+				self.O_optimizer.step()
 
-			P_loss = torch.mean(j_real) + gamma * torch.mean(torch.norm(vp_real)) + gamma * torch.mean(torch.norm(vp_fake))
-			P_loss.backward(retain_graph=True)
-			P_optimizer.step()
+				# --- Update C network --- #
+				self.C_optimizer.zero_grad()
 
-			L_loss = torch.mean(j_fake) + gamma * torch.mean(torch.norm(vl_real)) + gamma * torch.mean(torch.norm(vl_fake))
-			L_loss.backward(retain_graph=True)
-			L_optimizer.step()
+				z = torch.randn((self.opt.batch_size, self.opt.nz), device=self.opt.device)
+				x_fake = self.C(z)
+				v_fake = self.O(x_fake)
+				vp_fake = self.P(v_fake) + v_fake
+				vl_fake = self.L(v_fake) + v_fake
+				j_fake = self.J(torch.cat([vp_fake, vl_fake], dim=1))
 
-			# --- Update O network --- #
-			O_optimizer.zero_grad()
+				C_loss = self.criterion(j_fake, real_label)
+				C_loss.backward()
+				self.C_optimizer.step()
 
-			x_real = x[0].to(device)
-			v_real = O(x_real)
-			vp_real = P(v_real)
-			vl_real = L(v_real)
-			j_real = J(torch.cat([vp_real, vl_real], dim=1))
+				C_running_loss += C_loss.item()
+				O_running_loss += O_loss.item()
+				L_running_loss += L_loss.item()
+				P_running_loss += P_loss.item()
+			J_running_loss += J_loss.item()
 
-			z = torch.randn((batch_size, nz), device=device)
-			x_fake = C(z)
-			v_fake = O(x_fake)
-			vp_fake = P(v_fake)
-			vl_fake = L(v_fake)
-			j_fake = J(torch.cat([vp_fake, vl_fake], dim=1))
+		return C_running_loss / nitrs, O_running_loss / nitrs, L_running_loss / nitrs, P_running_loss / nitrs, J_running_loss / nitrs
 
-			loss_real = criterion(j_real, real_label)
-			loss_fake = criterion(j_fake, fake_label)
+	def generate(self):
+		self.C.eval()
+		return self.C(torch.randn((self.opt.batch_size, self.opt.nz), device=self.opt.device))
 
-			O_loss = loss_real + loss_fake
-			O_loss.backward()
-			O_optimizer.step()
-
-			O_running_loss = O_loss.item()
-			L_running_loss = L_loss.item()
-			P_running_loss = P_loss.item()
-		J_running_loss = J_loss.item()
-		C_running_loss = C_loss.item()
-
-	num_itrs = len(dataset) // batch_size
-	print('[epoch {:4d}] C: {:.4f}, O: {:.4f}, L: {:.4f}, P: {:.4f}, J: {:.4f}'.format(epoch, C_running_loss / num_itrs, O_running_loss / num_itrs, L_running_loss / num_itrs, P_running_loss / num_itrs, J_running_loss / num_itrs))
-	print('real: {:.4f}, fake: {:.4f}'.format(torch.mean(j_real).item(), torch.mean(j_fake).item()))
-	writer.add_scalars('Loss', {'C': C_running_loss / num_itrs, 'O': O_running_loss / num_itrs, "L": L_running_loss / num_itrs, "P": P_running_loss / num_itrs, "J": J_running_loss / num_itrs}, global_step=epoch)
+	def test(self):
+		generated_samples = []
+		for i in range(self.opt.num_test_samples // self.opt.batch_size):
+			generated_samples.append(self.generate())
+		return torch.cat(generated_samples).cpu().detach().numpy()
 
 
-# === test === #
-C.eval()
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--nz', type=int, default=64, help='latent variable size')
+	parser.add_argument('--ncf', type=int, default=32, help='feature size of criminal')
+	parser.add_argument('--njf', type=int, default=32, help='feature size of judge')
+	parser.add_argument('--njitrs', type=int, default=5, help='number of iterations for updating juddge')
+	parser.add_argument('--gamma', type=float, default=1.0, help='coefficient of perturbation loss')
 
-generated_samples = []
-for i in range(num_test_samples // batch_size):
-	z = torch.randn((batch_size, nz), device=device)
-	x_fake = C(z)
-	generated_samples.append(x_fake)
-np_generated_samples = torch.cat(generated_samples).cpu().detach().numpy()
+	parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+	parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam')
+	parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam')
+	parser.add_argument('--batch_size', type=int, default=8000, help='batch size')
+	parser.add_argument('--num_epochs', type=int, default=100, help='number of epochs')
+	parser.add_argument('--use_gpu', action='store_true', default=False, help='GPU mode')
 
-plt.scatter(np_data[:, 0], np_data[:, 1], s=5, c="red")
-plt.scatter(np_generated_samples[:, 0], np_generated_samples[:, 1], s=5, c="blue")
-plt.savefig(output_filename)
+	parser.add_argument('--log_dir', type=str, default='logs', help='log directory')
+	parser.add_argument('--num_test_samples', type=int, default=8000, help='number of samples in test')
+	parser.add_argument('--checkpoint', type=int, default=10, help='checkpoint epoch')
+	opt = parser.parse_args()
+
+	opt.device = torch.device("cuda:0" if opt.use_gpu else "cpu")
+	writer = SummaryWriter(os.path.join(opt.log_dir, 'runs'))
+
+	# dataset
+	tensor_data, dataset = generate_dataset()
+	np_data = tensor_data.numpy()
+	opt.loader = DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
+
+	model = JusticeGAN(opt)
+
+	for epoch in range(1, opt.num_epochs+1):
+		C_loss, O_loss, L_loss, P_loss, J_loss = model.train()
+
+		if epoch % opt.checkpoint == 0:
+			generated_samples = model.test()
+
+			plt.figure()		
+			plt.scatter(np_data[:, 0], np_data[:, 1], s=1, c="red")
+			plt.scatter(generated_samples[:, 0], generated_samples[:, 1], s=1, c="blue")
+			plt.axes().set_aspect('equal', 'datalim')
+			plt.savefig(os.path.join(opt.log_dir, 'epoch{:04d}.png'.format(epoch)), dpi=100)
+
+		print('[epoch {:4d}] C: {:.4f}, O: {:.4f}, L: {:.4f}, P: {:.4f}, J: {:.4f}'.format(epoch, C_loss, O_loss, L_loss, P_loss, J_loss))
+		writer.add_scalars('Loss', {'C': C_loss, 'O': O_loss, "L": L_loss, "P": P_loss, "J": J_loss}, global_step=epoch)
+
+
+
+def generate_dataset(radius=0.1, npoints=8, std=0.01, num_samples=10000):
+	means = []
+	for p in range(npoints):
+		theta = p * 2.0 * np.pi / npoints
+		means.append(torch.tensor([radius * float(np.cos(theta)), radius * float(np.sin(theta))]))
+	data = []
+	for i in range(npoints):
+		data.append(means[i] + std * torch.randn(num_samples, 2))
+	tensor_data = torch.cat(data)
+	dataset = TensorDataset(tensor_data)
+	return tensor_data, dataset
+
+
+if __name__ == '__main__':
+	main()
